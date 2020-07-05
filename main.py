0916784e8a5c59fb79bec7a5a5876fe825ef8137
Kamil Zdeb <kamilzdeb17@gmail.com>
2020-07-05 18:31:29 +0200

    Revert "Revert "Add mutations and roulette breeding""
    This reverts commit 7c67a0c1fdc59955b2d55382f8770b5665cb3eda.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from individual import Individual
 from item import Item
+from genetic_algorithm import print_population, generate_population
 import random
+
 
 def main():
     items = []
@@ -14,24 +15,13 @@ def main():
         weight += items[i].weight
     print(weight)
     max_weight = round(weight/2)
-    ind1 = Individual(items, max_weight)
-    ind1.generate_genotype()
-    ind2 = Individual(items, max_weight)
-    ind2.generate_genotype()
-    print(ind1.genotype)
-    print(ind2.genotype)
-    ind3 = Individual(items, max_weight)
-    ind3.breeding(ind1, ind2)
-    print(ind3.genotype)
-    ind4 = Individual(items, max_weight)
-    ind4.breeding(ind2, ind1)
-    print(ind4.genotype)
-    print('\n')
-    print("{}/{}".format(ind1.weight_used, max_weight))
-    print("{}/{}".format(ind2.weight_used, max_weight))
-    print("{}/{}".format(ind3.weight_used, max_weight))
-    print("{}/{}".format(ind4.weight_used, max_weight))
+    population = generate_population(10, max_weight, items)
+    print_population(population, max_weight)
+
+
+
 
 
 if __name__ == "__main__":
     main()
+
