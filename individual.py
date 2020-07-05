96188240ccd8bdddf63ecd72f72f66aaa45f593a
Kamil Zdeb <kamilzdeb17@gmail.com>
2020-07-05 14:41:47 +0200

    Add mutations and roulette breeding

--- individual.py
+++ individual.py
@@ -18,15 +18,21 @@ class Individual:
                 self.score += self.items[i].value
 
     def correct_genotype(self):
-        counter = 0
         for i in range(len(self.items)):
             if self.genotype[i]:
                 self.weight_used += self.items[i].weight
         while self.weight_used > self.max_weight:
-            if self.genotype[counter]:
-                self.genotype[counter] = False
-                self.weight_used -= self.items[counter].weight
-            counter += 1
+            active_chromosomes = []
+            for i in range(len(self.genotype)):
+                if self.genotype[i]:
+                    active_chromosomes.append(i)
+            selected_chromosome = random.choice(active_chromosomes)
+            self.genotype[selected_chromosome] = False
+            self.weight_used -= self.items[selected_chromosome].weight
+        # if self.genotype[counter]:
+            #     self.genotype[counter] = False
+            #     self.weight_used -= self.items[counter].weight
+            # counter += 1
 
     def breeding(self, ind_a, ind_b):
         for i in range(len(self.genotype)):
@@ -34,7 +40,13 @@ class Individual:
                 self.genotype[i] = ind_a.genotype[i]
             else:
                 self.genotype[i] = ind_b.genotype[i]
+        self.mutate()
         self.correct_genotype()
         for i in range(len(self.items)):
             if self.genotype[i]:
                 self.score += self.items[i].value
+
+    def mutate(self):
+        for i in range(len(self.genotype)):
+            if random.uniform(0, 1) < 0.02:
+                self.genotype[i] = not self.genotype[i]
